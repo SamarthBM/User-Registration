a9e0545cf324c215130a49dda1e5f2ab22a6df87
SamarthBM <samarthbm18@gmail.com>
2021-07-28 23:19:54 +0530

    Added: Method to validate all email samples

--- src/main/java/com/bridgelabs/UserRegistrationMain.java
+++ src/main/java/com/bridgelabs/UserRegistrationMain.java
@@ -9,7 +9,7 @@ public class UserRegistrationMain {
 
     public static void main(String[] args) {
         UserRegistrationImpl userRegistration = new UserRegistrationImpl();
-        userRegistration.validateUserDetails();
+        ValidateSampleMails.email();
 
     }
 }
