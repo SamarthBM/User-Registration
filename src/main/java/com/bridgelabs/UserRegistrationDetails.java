8b50a533dfbe68e9fcc50ed0c9dd4c789b22b2a7
SamarthBM <samarthbm18@gmail.com>
2021-07-28 13:29:32 +0530

    Added: Method to validate user's first name

---
+++ src/main/java/com/bridgelabs/UserRegistrationDetails.java
@@ -0,0 +1,17 @@
+package com.bridgelabs;
+
+public class UserRegistrationDetails {
+    String firstName;
+
+    public UserRegistrationDetails() {
+
+    }
+    // Generating getters and setters.
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+}
--- src/main/java/com/bridgelabs/UserRegistrationMain.java
+++ src/main/java/com/bridgelabs/UserRegistrationMain.java
@@ -3,7 +3,9 @@ package com.bridgelabs;
 public class UserRegistrationMain {
 
     public static void main(String[] args) {
-        System.out.println("Initial commit");
+        UserRegistrationImpl userRegistration = new UserRegistrationImpl();
+        userRegistration.validateUserDetails();
 
     }
 }
+
