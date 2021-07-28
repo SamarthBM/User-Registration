8926e4bc2aebead6c06900917745715c99e608a7
SamarthBM <samarthbm18@gmail.com>
2021-07-28 16:59:13 +0530

    Added: Method to validate mail ID

--- src/main/java/com/bridgelabs/UserRegistrationImpl.java
+++ src/main/java/com/bridgelabs/UserRegistrationImpl.java
@@ -1,5 +1,5 @@
 /****************************************************************
- * Purpose : Method to validate firs and last name using Regex.
+ * Purpose : Method to validate firs name, last name and mail ID using Regex.
  * @author Samarth BM
  ***************************************************************/
 package com.bridgelabs;
@@ -31,8 +31,19 @@ public class UserRegistrationImpl {
     }
 
     /*
-     * Purpose: Boolean method to validate first and last name. Condition for regex is, name
-     * should start with capital letter and name should have minimum 3 letters.
+     * Purpose: Boolean method to validate mail ID of user.
+     *
+     * @param mail: mail to validate.
+     */
+    public boolean validateEmail(String mail) {
+        String regex = "^([a-zA-z0-9-_+\\.]+)@([a-z0-9-]+)\\.([a-z,]{2,4})((\\.[a-z]{2,4})?)$";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(mail);
+        return matcher.matches();
+    }
+
+    /*
+     * Purpose: Boolean method to validate first name,last name and mail ID of user.
      */
     public void validateUserDetails() {
         Scanner sc = new Scanner(System.in);
@@ -57,6 +68,16 @@ public class UserRegistrationImpl {
             System.out.println("Last name is valid");
         else
             System.out.println("Last name is invalid");
+
+        // validating Email.
+        System.out.println("Enter your mail");
+        userDetails.setMail(sc.next());
+
+        boolean mail = userRegistration.validateEmail(userDetails.getMail());
+        if (mail)
+            System.out.println("Entered mail is valid");
+        else
+            System.out.println("Entered mail is invalid");
         sc.close();
     }
 }
